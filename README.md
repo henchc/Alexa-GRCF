e170a23f2596167b8d97eb1a28d2ad3568146914
Christopher Hench <chris.l.hench@gmail.com>
2018-01-05 09:51:16 -0800

    update code and txt files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# alexa-GRCF
+# Alexa-GRCF
+
+Simple Alexa skill that responds with the WOD at [Grassroots Crossfit](http://www.grassrootscrossfit.com/).
+
+Modified from the [Thornton Windchill Alexa Skill](https://github.com/kevinl95/Thornton-Windchill-Alexa-Skill)
--- testing.js
+++ testing.js
@@ -1,8 +1,8 @@
 
-var url = 'https://www.feederninja.com/api/content?type=rss&param=https%3A%2F%2Fbeyondthewhiteboard.com%2Fgyms%2F2755.atom&limit=10&nextUrl=';
-var http = require( 'https' );
+var url = 'https://www.feederninja.com/api/content?type=rss&param=https%3A%2F%2Fbeyondthewhiteboard.com%2Fgyms%2F2755.atom&limit=1000&nextUrl=';
+var https = require( 'https' );
 
-http.get( url, function( response ) {
+https.get( url, function( response ) {
 
     var data = '';
 
@@ -12,15 +12,16 @@ http.get( url, function( response ) {
 
         var json = JSON.parse( data );
 
-        var numParts = json.data.feeds.length
-        var date = json.data.feeds[0].date;
+        var numParts = json.data.feeds.length;
+        var unixTimeStamp = json.data.feeds[0].date;
+        var dateString = timeConverter(unixTimeStamp);
 
-        var text = '';
+        var text = 'This workout was posted on ' + dateString + '. ';
         var partCount = 1
         for(var i = 0; i < numParts - 1; i++) {
 
-    		var title = json.data.feeds[i].title;
-    		var desc = json.data.feeds[i].desc;
+    		var title = json.data.feeds[i].title.trim();
+    		var desc = json.data.feeds[i].desc.trim();
 
     		if (title.indexOf('posted')	== -1) {
     	    	text += 'Part ' + partCount + ' is ' + title + ': ' + desc + ' \n';
@@ -30,9 +31,24 @@ http.get( url, function( response ) {
         };
 
 
-        // console.log(text)
+        console.log(text)
 
-        output( text, context );
+        // output( text, context );
 
     } );
 } );
+
+// https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var day = days[a.getDay()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var time = day + ', ' + month + ' ' + date + ', ' + year + ' at ' + hour + ':' + min;
+  return time;
+}
